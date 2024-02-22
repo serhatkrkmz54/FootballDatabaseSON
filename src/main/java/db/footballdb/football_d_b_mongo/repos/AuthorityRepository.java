ffec747bb0bb8b09f52a10bed172c192712d6543
serhatkrkmz54 <korkmaz_serhat@hotmail.com>
2024-02-22 17:27:08 +0300

    duzenleme 22.02.2024

--- src/main/java/db/footballdb/football_d_b_mongo/repos/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package db.footballdb.football_d_b_mongo.repos;
-
-import db.footballdb.football_d_b_mongo.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuthorityRepository extends MongoRepository<User, Long> {
-
-}
